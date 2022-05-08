4de423957ad07f06d240f2ad3aaa675b17e2ac35
Enrique <enrique.costa@st-computacion.com>
2022-05-07 22:46:24 -0400

    add common django app

---
+++ apps/common/apps.py
@@ -0,0 +1,11 @@
+"""Common apps."""
+
+# Django
+from django.apps import AppConfig
+
+
+class CommonConfig(AppConfig):
+    """Common config model class."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.common'
---
+++ apps/common/models.py
@@ -0,0 +1,28 @@
+"""Common models."""
+
+# Standard libraries
+import uuid
+
+# Django
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """Base model class."""
+
+    pk_id = models.BigAutoField(
+        primary_key=True,
+        editable=False
+    )
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta options."""
+        abstract = True
+        ordering = ('-created', '-modified')
